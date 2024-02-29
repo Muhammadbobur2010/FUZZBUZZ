5926cd6a3c8837f13c6fa050094bce2b31805dfd
Muhammadbobur <sadriddinovmuhammadbobur@gmail.com>
2024-02-29 23:23:03 +0500

    FUZZBUZZ

---
+++ script.js
@@ -0,0 +1,11 @@
+let a =prompt("raqam kiriting");
+
+if (a % 3 ==0 && a % 5==0){
+    console.log("fuzzbuzz");
+}
+else if (a % 5 ==0){
+    console.log("buzz");
+}
+else if (a % 3 ==0){
+    console.log("fuzz");
+}
